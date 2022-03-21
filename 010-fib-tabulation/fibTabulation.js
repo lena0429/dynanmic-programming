dd08304d5531b4f2a53296fc60944d457003c0e8
Lena Hou <lenahou2013@gmail.com>
2022-03-21 11:26:52 -0400

    fib n with tabulation solution

---
+++ 010-fib-tabulation/fibTabulation.js
@@ -0,0 +1,19 @@
+const fibTabulation = (n) => {
+    const table = Array(n + 1).fill(0); // becase indices strat at 0;
+    table[1] = 1;
+
+    for (let i = 0; i <= n; i++) {
+        table[i + 1] += table[i]
+        table[i + 2] += table[i]
+    }
+    return table[n];
+}
+
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(50));
+
+
+// O(n) time
+// O(n) space
